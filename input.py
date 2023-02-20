74851b734316c72cbde3be99a4a97321e01e1aa3
Guilherme Nunes <dev.gnunes@gmail.com>
2023-02-20 12:06:08 -0300

    feat: input

---
+++ input.py
@@ -0,0 +1,13 @@
+# input(prompt)
+# REF.: https://docs.python.org/pt-br/3.11/library/functions.html?highlight=input#input
+
+# OBS.:
+# * Solicita ao usuário a digitação de um valor.
+# * Captura o valor digitado, pode ser atribuido a uma varivável como no exemplo abaixo.
+
+
+idade = input('Informe a sua idade: ')
+# Informe a sua idade: 
+
+print(f'Idade: {idade}')
+# Idade: (Valor digitado anteriormente)
