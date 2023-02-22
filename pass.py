3cac2d91770f20290cd1cba764a61246cf6b2d9c
Guilherme Nunes <dev.gnunes@gmail.com>
2023-02-22 20:41:19 -0300

    feat: pass

---
+++ pass.py
@@ -0,0 +1,13 @@
+# COMANDO PASS
+# REF.: https://docs.python.org/pt-br/3/tutorial/controlflow.html#pass-statements
+
+# O comando pass não faz nada. Pode ser usada quando a sintaxe exige um comando mas a semântica do programa não requer nenhuma ação.
+
+if (True):
+    pass
+
+while (True):
+    pass
+
+class MyEmptyClass:
+    pass
